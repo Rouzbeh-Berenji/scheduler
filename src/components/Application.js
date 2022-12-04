faa7e2f1831c598392dd83410d3e795f4f7a1c3c
Rouzbeh-Berenji <berenji.ro@gmail.com>
2022-12-04 17:29:01 -0500

    request Days from APIserver using effect and axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index";
 import "components/Application.scss";
-import { useState } from "react";
+import axios from "axios";
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 const appointments = {
   "1": {
     id: 1,
@@ -64,6 +47,15 @@ const appointments = {
 
 export default function Application(props) {
   const [day,setDay] = useState("Monday")
+  const [days, setDays] = useState([]);
+
+
+useEffect(() => {
+  axios.get('http://localhost:8001/api/days')
+    .then((response) => {setDays (response.data)})
+}, [])
+
+
   return (
     <main className="layout">
       <section className="sidebar">
